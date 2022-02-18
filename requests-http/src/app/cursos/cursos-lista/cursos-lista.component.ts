a5575541a97e414521a5618487c6827bff33930e
GustavoBomfim <gustavosoaresbomfim@hotmail.com>
2022-02-18 15:32:17 -0300

    Variável de Ambiente

--- requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,15 +1,61 @@
+import { preserveWhitespacesDefault } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { CursosService } from './cursos.service';
+//preserveWhitespacesDefault: true
+import { Observable, empty, of, Subject, EMPTY } from 'rxjs';
+import { catchError, switchMap } from 'rxjs/operators';
+import { Curso } from './curso';
+
 
 @Component({
   selector: 'app-cursos-lista',
   templateUrl: './cursos-lista.component.html',
-  styleUrls: ['./cursos-lista.component.scss']
+  styleUrls: ['./cursos-lista.component.scss'],
+  preserveWhitespaces: true
 })
 export class CursosListaComponent implements OnInit {
 
-  constructor() { }
+  //cursos!: Curso[];
+  cursos$!: Observable<Curso[]>;
+  error$ = new Subject<boolean>();
 
-  ngOnInit(): void {
+  constructor(private service: CursosService) { }
+
+  ngOnInit() {
+    // this.service.list()
+    //   .subscribe(dados => this.cursos = dados);
+
+    this.onRefresh();
+  }
+
+  onRefresh() {
+     this.cursos$ = this.service.list()
+     .pipe(
+       // map(),
+       // tap(),
+       // switchMap(),
+       catchError(error => {
+         console.error(error);
+         this.error$.next(true);
+         return EMPTY;
+       })
+     );
+
+  //    this.service.list()
+  //    .pipe(
+  //      catchError(error => empty())
+  //    )
+  //    .subscribe(
+  //      dados => {
+  //        console.log(dados);
+  //      }
+  //      // ,error => console.error(error),
+  //     // () => console.log('Obserservable completo!')
+  //    );
+  // 
   }
 
 }
+
+
+
---
+++ requests-http/src/app/cursos/cursos-lista/cursos.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Curso } from './curso';
+import { tap } from 'rxjs/operators';
+
+import { environment } from 'src/environments/environment';
+@Injectable({
+  providedIn: 'root'
+})
+export class CursosService {
+
+  private readonly API = `${environment.API}cursos`;
+
+  constructor(private http: HttpClient) { }
+
+  list(){
+
+    return this.http.get<Curso[]>(this.API);
+  //   return this.http.get<Curso[]>(this.API)
+  //     .pipe(
+  //       delay(2000),
+  //       tap(console.log)
+  //     );
+  }
+}
